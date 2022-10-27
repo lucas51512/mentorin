7e524a362ee19f03336313f98a60813cc423e5f9
Lucas <56892062+lucas51512@users.noreply.github.com>
2022-10-27 11:57:04 -0300

    Update create-empresa.dto.ts

--- backend/src/empresa/dto/create-empresa.dto.ts
+++ backend/src/empresa/dto/create-empresa.dto.ts
@@ -1,1 +1,7 @@
-export class CreateEmpresaDto {}
+export class CreateEmpresaDto {
+    idEmpresa: number
+    nomeFantasiaEmpresa: string
+    telefoneEmpresa: string
+    emailContaEmpresa: string
+    cnpjEmpresa: string
+}
