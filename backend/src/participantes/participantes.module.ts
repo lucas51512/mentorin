c91a598689e390ea35af2f73e4f278ee91b82583
Lucas <56892062+lucas51512@users.noreply.github.com>
2022-10-27 12:01:57 -0300

    Update participantes.module.ts

--- backend/src/participantes/participantes.module.ts
+++ backend/src/participantes/participantes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ParticipantesService } from './participantes.service';
 import { ParticipantesController } from './participantes.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [ParticipantesController],
-  providers: [ParticipantesService]
+  providers: [ParticipantesService, PrismaService]
 })
 export class ParticipantesModule {}
