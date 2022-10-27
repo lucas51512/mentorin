efb08ce1eee765128cd50651e9d8d0865bb647b5
Lucas <56892062+lucas51512@users.noreply.github.com>
2022-10-27 11:53:00 -0300

    Update categoria-interna.service.ts

--- backend/src/categoria-interna/categoria-interna.service.ts
+++ backend/src/categoria-interna/categoria-interna.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCategoriaInternaDto } from './dto/create-categoria-interna.dto';
 import { UpdateCategoriaInternaDto } from './dto/update-categoria-interna.dto';
 
 @Injectable()
 export class CategoriaInternaService {
-  create(createCategoriaInternaDto: CreateCategoriaInternaDto) {
-    return 'This action adds a new categoriaInterna';
+  constructor (private readonly prisma: PrismaService) {}
+
+  async create(createCategoriaInternaDto: CreateCategoriaInternaDto) {
+    return await this.prisma.categoriaInterna.create({
+      data: {
+      ...createCategoriaInternaDto
+      }
+    });
   }
 
-  findAll() {
-    return `This action returns all categoriaInterna`;
+  async findAll() {
+    return await this.prisma.categoriaInterna.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} categoriaInterna`;
+  async findOne(id: number) {
+    return await this.prisma.categoriaInterna.findUnique({
+      where: {
+        idCategoria: id
+      }
+    });
   }
 
-  update(id: number, updateCategoriaInternaDto: UpdateCategoriaInternaDto) {
-    return `This action updates a #${id} categoriaInterna`;
+  async update(id: number, data: UpdateCategoriaInternaDto) {
+    return this.prisma.categoriaInterna.update({
+      where: {
+        idCategoria: id,
+      },
+      data
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} categoriaInterna`;
+  async remove(id: number) {
+    return this.prisma.categoriaInterna.delete({
+      where: {
+        idCategoria: id
+      }
+    });
   }
 }
