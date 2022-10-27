35e6a1074d6f5abd3c423e93f84a3315814db574
Lucas <56892062+lucas51512@users.noreply.github.com>
2022-10-27 11:52:16 -0300

    Update categoria-interna.module.ts

--- backend/src/categoria-interna/categoria-interna.module.ts
+++ backend/src/categoria-interna/categoria-interna.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoriaInternaService } from './categoria-interna.service';
 import { CategoriaInternaController } from './categoria-interna.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [CategoriaInternaController],
-  providers: [CategoriaInternaService]
+  providers: [CategoriaInternaService, PrismaService]
 })
 export class CategoriaInternaModule {}
