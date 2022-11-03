45a8bd1d9c64dddb02b6916e388879ad16c0afd1
Lucas <lucasbrancalhao454@gmail.com>
2022-11-03 10:44:40 -0300

    Cors adicionado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { cors } from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   app.setGlobalPrefix("api/v1");
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
