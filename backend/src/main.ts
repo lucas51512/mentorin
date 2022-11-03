426adc5a97bf3bf40e2928811b6ccc95bcbc1c74
Lucas <lucasbrancalhao454@gmail.com>
2022-11-03 10:50:43 -0300

    Cors corrigido

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { cors } from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix("api/v1");
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
